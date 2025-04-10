9b0ee4603c1743ef98af97b8abea29c17fcc4e4d
Rayssa Costa <115506404+RayssaIssa@users.noreply.github.com>
2025-04-10 07:44:40 -0300

    Delete AP2/lista4 directory

--- AP2/lista4/DOIS.h
+++
@@ -1,8 +0,0 @@
-
-int POSITIVO_NEGATIVO(int n){
-    if(n > 0){
-        return 1;
-    }else{
-        return 0;
-    }
-}
--- AP2/lista4/QUATRO.h
+++
@@ -1,8 +0,0 @@
-
-int soma(int x, int y){
-    int soma_entre_eles = 0, i;
-    for(i = x; i <= y; i++){
-        soma_entre_eles += i;
-    }
-    return soma_entre_eles;
-}
